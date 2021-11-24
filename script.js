71a46637a297ab32a1c8ad32ac4409f1d8abb2d4
YonasValentin <eaayokli@students.eaaa.dk>
2021-11-24 23:29:39 +0100

    all validation added

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const bodyInputError = document.getElementById('contact-message-error');
 // get form element
 const contactForm = document.getElementById('contact-form');
 // listen for form submit event
-contactForm.addEventListener('submit', saveFormData);
+contactForm.addEventListener('submit', validateFormData);
 
-function saveFormData(event) {
+function validateFormData(event) {
     event.preventDefault();
 
     const formData = new FormData(event.target);
@@ -21,10 +21,40 @@ function saveFormData(event) {
     const subjectInput = formData.get('subject').trim();
     const messageInput = formData.get('message').trim();
 
+    // empty name validation
     if (!nameInput) {
         nameInputError.innerText = 'Please enter your name';
-    } else if (nameInput) {
+    } else {
         nameInputError.innerText = '';
-        nameInputError.classList.remove('contact-name-error');
+    }
+    // empty email validation
+    if (!emailInput) {
+        emailInputError.innerText = 'Please enter your email address';
+        // email validation
+    } else if (!emailInput.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        emailInputError.innerText = 'Please enter a valid email address';
+    } else {
+        emailInputError. innerText = '';
+    }
+    // empty phone validation
+    if (!phoneInput) {
+        phoneInputError.innerText = 'Please enter your phone number';
+        // phone number validation
+    } else if (phoneInput.toString().length <= 8 && !phoneInput.toString().match(/^[0-9]+$/)) {
+        phoneInputError.innerText = 'Please enter a valid phone number';
+    } else {
+        phoneInputError. innerText = '';
+    }
+    // empty subject validation
+    if (!subjectInput) {
+        subjectInputError.innerText = 'Please enter your subject';
+    } else {
+        subjectInputError. innerText = '';
+    }
+    // empty message validation
+    if (!messageInput) {
+        messageInputError.innerText = 'Please enter your message';
+    } else {
+        messageInputError.innerText = '';
     }
 }
