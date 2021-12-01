a2110868c5e4d073bb9ebdc27a88ab14332e8ec5
YonasValentin <eaayokli@students.eaaa.dk>
2021-12-01 16:37:27 +0100

    [fix] responsive slick-slider

--- js/cases-slider.js
+++ js/cases-slider.js
@@ -1,8 +1,33 @@
 $(document).ready(function () {
   $(".cases-slider").slick({
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
