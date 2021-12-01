ae0ea8faabc83bc7388d61d90211b73fecefe3a2
YonasValentin <eaayokli@students.eaaa.dk>
2021-12-01 19:39:06 +0100

    [added] DOM manipulation | arrows customise

--- js/cases-slider.js
+++ js/cases-slider.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   $(".cases-slider").slick({
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: true,
