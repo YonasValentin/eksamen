561ec415fe404ae2e2a64a41318455b9de26a295
YonasValentin <eaayokli@students.eaaa.dk>
2021-12-03 13:31:41 +0100

    [added] scrollIntoView

--- js/scroll-behavior.js
+++ js/scroll-behavior.js
@@ -4,3 +4,10 @@ $(document).ready(function () {
     scrollSpeed: 1100,
   });
 });
+
+let omOs = document.getElementById("nav-om-os");
+let section = document.getElementById("section-om-os");
+
+omOs.addEventListener("click", function () {
+  section.scrollIntoView(true, { behavior: "smooth" });
+});
