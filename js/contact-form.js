52a963d366c0e0b8e5b6dda7259420b8de121e50
YonasValentin <eaayokli@students.eaaa.dk>
2021-12-03 01:27:07 +0100

    [clear] new structure added

---
+++ js/contact-form.js
@@ -0,0 +1,162 @@
+// error id
+const contactFormError = document.getElementById("contact-form-error");
+const nameInputError = document.getElementById("contact-name-error");
+const emailInputError = document.getElementById("contact-email-error");
+const phoneInputError = document.getElementById("contact-phone-error");
+const subjectInputError = document.getElementById("contact-subject-error");
+const messageInputError = document.getElementById("contact-message-error");
+const dateInputError = document.getElementById("contact-date-error");
+const timeInputError = document.getElementById("contact-time-error");
+
+// get form element
+const contactForm = document.getElementById("contact-form");
+// listen for form submit event
+contactForm.addEventListener("submit", validateFormData);
+// function execute, when form submit is clicked
+function validateFormData(event) {
+  // prevent default HTML behavior
+  event.preventDefault();
+  // get form input data
+  const formData = new FormData(this);
+  // get input fields values
+  const nameInput = formData.get("name").trim();
+  const emailInput = formData.get("email").trim();
+  const phoneInput = formData.get("phone").trim();
+  const subjectInput = formData.get("subject").trim();
+  const messageInput = formData.get("message").trim();
+  const dateInput = formData.get("date");
+  const timeInput = formData.get("time");
+
+  // check if all fields are valid
+  if (
+    !nameInput ||
+    !emailInput ||
+    !phoneInput ||
+    !subjectInput ||
+    !messageInput ||
+    !dateInput ||
+    !timeInput
+  ) {
+    contactFormError.innerText = "Venligst udfyld alle felterne";
+    // execute fetch POST method to formsubmit
+  } else {
+    fetch("https://formsubmit.co/ajax/yonasklibi@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        From: nameInput,
+        Email: emailInput,
+        Phone: phoneInput,
+        Subject: subjectInput,
+        Message: messageInput,
+      }),
+      // checks if form is submitted correctly
+    })
+      .then(function (response) {
+        response.json();
+        // console.log response if success or error
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    // if multiple admin emails
+    fetch("https://formsubmit.co/ajax/yonasmougaard@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        From: nameInput,
+        Email: emailInput,
+        Phone: phoneInput,
+        Subject: subjectInput,
+        Message: messageInput,
+      }),
+      // checks if form is submitted correctly
+    })
+      .then(function (response) {
+        response.json();
+        // console.log response if success or error
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  // check if empty name input field
+  if (!nameInput) {
+    nameInputError.innerText = "Navn er påkrævet";
+    // if a value is entered, then remove error message
+  } else {
+    nameInputError.innerText = "";
+  }
+  // check if empty email input field
+  if (!emailInput) {
+    emailInputError.innerText = "E-mail er påkrævet";
+    // check is email address is valid
+  } else if (
+    !emailInput.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    emailInputError.innerText = "Din e-mail er ugyldig";
+    // if email address is entered and valid, then remove error message
+  } else {
+    emailInputError.innerText = "";
+  }
+  // check if empty phone input field
+  if (!phoneInput) {
+    phoneInputError.innerText = "Tlf. nr. er påkrævet";
+    // check if phone number starts with either +45 || 0045 && contains 8 digtets
+  } else if (
+    !phoneInput.match(/^[+]*[45]{2}[\d]{8}|^[0045]{4}[\d]{8}|^\d{8}$/)
+  ) {
+    phoneInputError.innerText = "Dit tlf. nr er ugyldigt";
+    // if phone number is entered and valid, then remove error message
+  } else {
+    phoneInputError.innerText = "";
+  }
+  // check if empty subject input field
+  if (!subjectInput) {
+    subjectInputError.innerText = "Emne er påkrævet";
+    // check if subject is more or equal to 10 characters
+  } else if (subjectInput.length <= 10) {
+    subjectInputError.innerText = "Emnet skal mindst indholde 10 tegn";
+    // if subject is entered and valid, then remove error message
+  } else {
+    subjectInputError.innerText = "";
+  }
+  // check if empty message input field
+  if (!messageInput) {
+    messageInputError.innerText = "Meddelse er påkrævet";
+    // check if message is more or equal to 30 characters
+  } else if (messageInput.length <= 30) {
+    messageInputError.innerText = "Meddelsen skal mindst indholde 30 tegn";
+    // if message is entered and valid, then remove error message
+  } else {
+    messageInputError.innerText = "";
+  }
+  // check if date input field is entered
+  if (!dateInput) {
+    dateInputError.innerText = "Dato er påkrævet";
+    // if date is entered, then remove error message
+  } else {
+    dateInputError.innerText = "";
+  }
+  // check if time input field is entered
+  if (!timeInput) {
+    timeInputError.innerText = "Tid er påkrævet";
+    // if time is entered, then remove error
+  } else {
+    timeInputError.innerText = "";
+  }
+}
---
+++ js/slick-slider.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  $(".cases-slider").slick({
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
