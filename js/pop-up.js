467b42d35262e3a62e1a7c0dd144aab6cef97e70
YonasValentin <eaayokli@students.eaaa.dk>
2021-12-06 00:52:24 +0100

    [added] comments added

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,17 +1,23 @@
+// find pop-up block in HTML
 const popUp = document.getElementById("");
 
+// find btn to open block element
 const popUpBtn = document.getElementById("");
 
+// find btn to close block element
 const popUpClose = document.getElementById("");
 
+// show block, when click on popUpBtn
 popUpBtn.addEventListener("click", function () {
   popUp.style.display.block;
 });
 
+// close block, when click on popUpClose
 popUpClose.addEventListener("click", function () {
   popUp.style.display.none;
 });
 
+// close block, when click outside of the block
 window.addEventListener("click", function (e) {
   if (e.target == popUp) {
     popUp.style.display.none;
