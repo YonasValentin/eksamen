f95f1a3158dd3adcb6d005ab84df876a34529845
YonasValentin <eaayokli@students.eaaa.dk>
2021-12-05 23:36:34 +0100

    [working] array now fully functional

--- js/project-arrays.js
+++ js/project-arrays.js
@@ -1,5 +1,5 @@
 // array of projekt 1
-const projekt1Array = [
+let projekt1Array = [
   {
     title: "Mark",
     quote: "Incididunt nostrud et minim in excepteur culpa anim.",
@@ -22,14 +22,15 @@ const projekt1Array = [
   },
 ];
 
-const projekt1PrevBtn = document.getElementById("section-projekt-1__prev-btn");
-const projekt1NextBtn = document.getElementById("section-projekt-1__next-btn");
+let projekt1PrevBtn = document.getElementById("section-projekt-1__prev-btn");
+let projekt1NextBtn = document.getElementById("section-projekt-1__next-btn");
 
-const projekt1Img = document.getElementById("section-projekt-1__img");
-const projekt1Title = document.getElementById("section-projekt-1__info-title");
-const projekt1Quote = document.getElementById("section-projekt-1__info-quote");
+let projekt1Img = document.getElementById("section-projekt-1__img");
+let projekt1Title = document.getElementById("section-projekt-1__info-title");
+let projekt1Quote = document.getElementById("section-projekt-1__info-quote");
 // defindes the data-index to start
-const i = 0;
+let i = 0;
+let items = projekt1Array[i];
 
 projekt1Img.src = projekt1Array[i].image;
 projekt1Title.innerText = projekt1Array[i].title;
@@ -37,9 +38,19 @@ projekt1Quote.innerText = projekt1Array[i].quote;
 
 projekt1Img.style.width = "100px";
 
+projekt1PrevBtn.addEventListener("click", function () {
+  i--;
+  if (i < projekt1Array.length - 1) {
+    i = 0;
+  }
+  projekt1Img.src = projekt1Array[i].image;
+  projekt1Title.innerText = projekt1Array[i].title;
+  projekt1Quote.innerText = projekt1Array[i].quote;
+});
+
 projekt1NextBtn.addEventListener("click", function () {
   i++;
-  if (i > projekt1Array.length - 1) {
+  if (i > projekt1Array.length + 1) {
     i = 0;
   }
   projekt1Img.src = projekt1Array[i].image;
