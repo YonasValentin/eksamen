7b77253e673d7ac3b720d983db6c5c9a9023d6d8
Banntex <danielrk12345@hotmail.com>
2021-12-06 15:24:31 +0100

    added slick slider to slideshow

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -34,3 +34,40 @@ $(document).ready(function () {
     ],
   });
 });
+
+$(document).ready(function () {
+  $(".oevrige-projekter").slick({
+    centerMode: true,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
