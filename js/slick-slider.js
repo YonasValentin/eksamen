77762a2023cbf2fdf2f655cd16e8882e3d5bfa2f
YonasValentin <eaayokli@students.eaaa.dk>
2021-12-13 11:47:14 +0100

    [added] artikler tilføjet

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -36,21 +36,39 @@ $(document).ready(function () {
   });
 });
 
-// slider syncing
-$document.ready(function () {
-  $(".slider-for").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: ".slider-nav",
-  });
-  $(".slider-nav").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: ".slider-for",
-    dots: true,
+$(document).ready(function () {
+  $("#section-inner-artikler").slick({
     centerMode: true,
-    focusOnSelect: true,
+    prevArrow: $(".prev-artikler"),
+    nextArrow: $(".next-artikler"),
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
